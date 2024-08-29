41afb1aec00dffa47174cace438eda4fce9a5d7b
Jayden Wilson <Jayden@tabber.co.uk>
2024-08-29 12:44:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Software_Development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
